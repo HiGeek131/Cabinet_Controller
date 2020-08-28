ea73d2d5a8f51b14cc8f1297750ad63ad54de187
Gary <2801079570@qq.com>
2020-08-28 22:36:06 +0800

    加入SQL相关程序

--- src/Main.java
+++ src/Main.java
@@ -16,10 +16,48 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 import java.io.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 
 public class Main {
+    public static final String db_url = "jbdc:mysql://192.168.2.221:3306/gary_db?useSSL=false";
+    public static final String db_user = "test";
+    public static final String sb_password = "";
     public static void main(String[] args) {
         System.out.println(FileTool.readFile("/sys/class/thermal/thermal_zone0/temp"));
+        MyThread myThread = new MyThread("test1");
+        myThread.start();
+    }
+}
+
+class MyThread extends Thread {
+    private String name;
+    public MyThread(String name) {
+        this.name = name;
+    }
+    @SuppressWarnings("InfiniteLoopStatement")
+    public void run() {
+        InetAddress address = null;
+        String hostName = "";
+        try {
+            address = InetAddress.getLocalHost();
+            hostName = address.getHostName().toString();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        if (! hostName.isEmpty()) {
+            System.out.println("Host Name:" + hostName);
+        }
+
+        while (true) {
+            System.out.println(this.name + "RUN");
+            
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
